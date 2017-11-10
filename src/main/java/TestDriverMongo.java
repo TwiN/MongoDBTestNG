2480661f7273e825046b0e2608b40cf8e93af53a
TwinProduction <christian-ct@hotmail.com>
2017-11-10 11:35:47 -0500

    Converted tests from JUnit to TestNG

--- src/main/java/TestDriverMongo.java
+++ src/main/java/TestDriverMongo.java
@@ -6,36 +6,38 @@ import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Projections;
 import com.mongodb.client.model.Updates;
 import org.bson.Document;
+import org.testng.annotations.*;
 
-import org.junit.Before;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
-import org.junit.runners.MethodSorters;
-
-import static org.junit.Assert.assertEquals;
+import static org.testng.Assert.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+
 public class TestDriverMongo {
 	
-	private MongoClient client = new MongoClient("localhost", 32768);
-	private MongoDatabase db = client.getDatabase("test");
-	private MongoCollection<Document> collection = db.getCollection("test");
+	private static final String MONGO_SERVER_IP   = "localhost";
+	private static final int    MONGO_SERVER_PORT = 32768;
+	private static final String DATABASE_NAME     = "test";
+	private static final String COLLECTION_NAME   = "test";
+	
+	private MongoClient client = null;
+	private MongoDatabase db = null;
+	private MongoCollection<Document> collection = null;
 	
 	
-	public void printOutput(FindIterable<Document> output) {
-		for (Document doc : output) {
-			System.out.println(doc.toJson());
-		}
+	@BeforeClass
+	public void initialSetup() {
+		client = new MongoClient(MONGO_SERVER_IP, MONGO_SERVER_PORT);
+		db = client.getDatabase(DATABASE_NAME);
+		collection = db.getCollection(COLLECTION_NAME);
+		collection.drop();
 	}
 	
 	
 	@Test
 	public void testInsert() {
-		collection.drop();
 		Document user = new Document("name", "user-01")
 			  .append("email", "user-01@example.com")
 			  .append("salary", 52000)
@@ -86,7 +88,7 @@ public class TestDriverMongo {
 	}
 	
 	
-	@Test
+	@Test(dependsOnMethods={"testInsert"})
 	public void testQuerySalary() {
 		FindIterable<Document> mongoIter = collection.find(Filters.and(Filters.gte("salary", 30000), Filters.lte("salary", 50000)));
 		assertEquals(2, mongoIter.into(new ArrayList<Document>()).size());
@@ -94,7 +96,7 @@ public class TestDriverMongo {
 	}
 	
 	
-	@Test
+	@Test(dependsOnMethods={"testInsert"})
 	public void testQueryGender() {
 		FindIterable<Document> mongoIter = collection.find(Filters.eq("infoUser.gender", "F"));
 		assertEquals(2, mongoIter.into(new ArrayList<Document>()).size());
@@ -102,7 +104,7 @@ public class TestDriverMongo {
 	}
 	
 	
-	@Test
+	@Test(dependsOnMethods={"testInsert"})
 	public void testQuerySkills() {
 		FindIterable<Document> mongoIter = collection.find(Filters.ne("skills", "DEV"));
 		assertEquals(2, mongoIter.into(new ArrayList<Document>()).size());
@@ -110,7 +112,7 @@ public class TestDriverMongo {
 	}
 	
 	
-	@Test
+	@Test(dependsOnMethods={"testInsert"})
 	public void testQuery() {
 		FindIterable<Document> mongoIter = collection.find(Filters.eq("name", "user-01")).projection(Projections.include("email", "salary"));
 		assertEquals(1, mongoIter.into(new ArrayList<Document>()).size());
@@ -118,12 +120,24 @@ public class TestDriverMongo {
 	}
 	
 	
-	@Test
+	@Test(dependsOnMethods={"testInsert"})
 	public void testUpdate() {
 		collection.updateOne(Filters.eq("name", "user-02"), Updates.set("salary", 1337));
 		FindIterable<Document> mongoIter = collection.find(Filters.and(Filters.eq("name", "user-02"), Filters.eq("salary", 1337)));
 		assertEquals(1, mongoIter.into(new ArrayList<Document>()).size());
 		printOutput(mongoIter);
+	}
+	
+	
+	//////////////////////
+	// NON-TEST METHODS //
+	//////////////////////
+	
+	
+	public void printOutput(FindIterable<Document> output) {
+		for (Document doc : output) {
+			System.out.println(doc.toJson());
+		}
 	}
 	
 }
